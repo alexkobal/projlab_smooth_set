4196cda265f6990703dede4c4b8cdd265f1db558
SonOf1998 <40013370+SonOf1998@users.noreply.github.com>
2019-03-18 15:33:59 +0100

    List<Tile> neighbors is protected from now on

--- src/Tile.java
+++ src/Tile.java
@@ -1,9 +1,81 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Tile {
-    private ArrayList<Tile> neighbors;
+public class Tile
+{
+    protected AThing contains;          //protected, hogy a BrokenTile ne getteljen/setteljen
+    protected List<Tile> neighbors;
 
-    public ArrayList<Tile> getNeighbors(){
+    public Tile()
+    {
+        contains = null;
+        neighbors = new ArrayList<Tile>();
+    }
+
+
+    public boolean placeThing(AThing thing)
+    {
+        if(contains != null)
+        {
+            Main.printer.functionCall(contains.name, "hitby", thing.name);
+            boolean res = contains.hitBy(thing);
+            Main.printer.returnFromFunctionCall();
+
+            return res;
+        }
+
+        // UNDER CONSTRUCTION
+
+        return false;
+    }
+
+    /*
+    * Minden nem üres szomszédot értesítünk, hogy a rajtunk lévő AThing (itt most Armchair, GameMachine, VendingMachine)
+    * akar valamit (=lejátszódik az effect() függvénye)
+    *
+    */
+    public void notifyNeighbors()
+    {
+        for(Tile neighbor : neighbors)
+        {
+            if(neighbor.getContains() != null)
+            {
+                Main.printer.functionCall("nt", "placeThing");
+                placeThing(contains);
+                Main.printer.returnFromFunctionCall();
+            }
+        }
+    }
+
+    // virtuális függvény, hogy a sima tile kompatibilis legyen a brokentile-lal
+    public void loseLife(){}
+
+    // Az adott csempét elfelejtetjük a szomszédokkal
+    public void unlink()
+    {
+        for(Tile neighbor : neighbors)
+        {
+            neighbor.getNeighbors().remove(neighbor);
+        }
+    }
+
+    public List<Tile> getNeighbors()
+    {
         return neighbors;
+    }
+
+
+    public void addNeighbor(Tile tile)
+    {
+        neighbors.add(tile);
+    }
+
+    public void setContains(AThing thing)
+    {
+        contains = thing;
+    }
+
+    public AThing getContains()
+    {
+        return contains;
     }
 }
