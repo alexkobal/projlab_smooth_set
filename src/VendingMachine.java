99460e813fb27576b368096ed5cb4d170738fbbd
Mmate99 <47672523+Mmate99@users.noreply.github.com>
2019-03-18 15:54:05 +0100

    Update VendingMachine.java

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,2 +1,11 @@
-public class VendingMachine {
+public class VendingMachine extends AThing implements IPandaEffective{
+
+	private int beepTime;
+	
+	public VendingMachine(int t) { beepTime = t; }
+	
+	public void effect()
+	{
+		isOn.notifyNeighbours();
+	}
 }
