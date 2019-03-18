5142e0937b50154f3fd4eab0824cfb12b50b90ff
Mmate99 <47672523+Mmate99@users.noreply.github.com>
2019-03-18 15:52:26 +0100

    Update Armchair.java

--- src/Armchair.java
+++ src/Armchair.java
@@ -1,2 +1,16 @@
-public class Armchair {
+public class Armchair extends AThing implements IPandaEffective{
+
+	private LazyPanda panda;
+	
+	public void effect()
+	{
+		isOn.notifyNeighbours();
+	}
+	
+	public boolean isOccupied()
+	{
+		if(panda != null)
+			return true;
+		return false;
+	}
 }
