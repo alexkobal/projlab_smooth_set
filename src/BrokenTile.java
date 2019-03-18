60a53acd6c21b3afec4dc9c98aef5c025b23fb22
SonOf1998 <40013370+SonOf1998@users.noreply.github.com>
2019-03-18 15:28:26 +0100

    Add files via upload

---
+++ src/BrokenTile.java
@@ -0,0 +1,24 @@
+public class BrokenTile extends Tile
+{
+    private int lifeTime;
+
+    public BrokenTile()
+    {
+        lifeTime = 20;
+    }
+
+    public void loseLife()
+    {
+        lifeTime--;
+        if(lifeTime == 0)
+        {
+            Main.printer.functionCall("bt", "unlink");
+            unlink();
+            Main.printer.returnFromFunctionCall();
+
+            Main.printer.functionCall(contains.name, "kill");
+            contains.kill();
+            Main.printer.returnFromFunctionCall();
+        }
+    }
+}
