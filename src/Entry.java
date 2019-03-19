553d7dbb5d722e7effe35ab25ef592dbbda2ef41
alexkobal <alexkobal@live.ru>
2019-03-19 11:46:52 +0100

    JavaDoc comments added to Entry, Exit and Wardrobe classes.

--- src/Entry.java
+++ src/Entry.java
@@ -1,21 +1,59 @@
-public class Entry {
+/**
+ * Entry
+ * <p>
+ *     This class extends AThing. It represents the entry point of the game.
+ *     This class generates new Orangutans on the floor.
+ * </p>
+ */
+public class Entry extends AThing{
 
-    private Tile entryTile;
-    private int orangutansToPush;
+	/**
+	 * entryTile
+	 * <p>
+	 *     This is the Tile where Entry places an Orangutan.
+	 * </p>
+	 */
+	private Tile entryTile;
+	/**
+	 * orangutansToPush
+	 * <p>
+	 *     Number of orangutans to push to the floor.
+	 * </p>
+	 */
+	private int orangutansToPush;
+	/**
+	 * pushOrangutan
+	 * <p>
+	 *     This function places an orangutan on the floor.
+	 * </p>
+	 */
+	public void pushOrangutan(){
+		Orangutan orangutan = new Orangutan();
+		orangutan.move(entryTile);
+	}
 
-    public void pushOrangutan(){
-        Orangutan orangutan = new Orangutan();
-        orangutan.move(entryTile);
-    }
-
-    public void addOrangutan(int n){
-        orangutansToPush += n;
-    }
-
-    public void nextTurn(){
-        if(orangutansToPush > 0){
-            pushOrangutan();
-            orangutansToPush--;
-        }
-    }
+	/**
+	 * addOrangutan
+	 * <p>
+	 *     Adds orangutans to place on the floor.
+	 * </p>
+	 * @param n is the number of Orangutans to add for pushing.
+	 */
+	public void addOrangutan(int n){
+		orangutansToPush += n;
+	}
+	/**
+	 * nextTurn
+	 * <p>
+	 *     This function is called on every turn,
+	 *     it places one Orangutan on the floor,
+	 *     then decrements the number of Orangutans to push.
+	 * </p>
+	 */
+	public void nextTurn(){
+		if(orangutansToPush > 0){
+			pushOrangutan();
+			orangutansToPush--;
+		}
+	}
 }
--- src/Exit.java
+++ src/Exit.java
@@ -1,14 +1,45 @@
+/**
+ * Exit
+ * <p>
+ *     This class extends AThing. It represents the exit point of the floor.
+ * </p>
+ */
 public class Exit extends AThing {
-
+    /**
+     * nextPanda
+     * <p>
+     *     This field is a reference for next Panda that should go through the exit.
+     *     It matters when an Orangutan leads a chain of Pandas to the exit.
+     * </p>
+     */
     private Panda nextPanda;
-
+    /**
+     * hitBy Orangutan
+     * <p>
+     *     This function overrides the base function and it is called when an Orangutan tries to exit.
+     * </p>
+     * @param  orangutan is the Thing which collided with this object.
+     * @return returns true if the orangutan succeeded to exit.
+     */
     @Override
     public boolean hitBy(Orangutan orangutan) {
-        nextPanda = (Panda) orangutan.getNextAnimal();
-        nextPanda.setPrevAnimal(null);
-        orangutan = null;
-        return true;
+        if(nextPanda == null) {
+            nextPanda = (Panda) orangutan.getNextAnimal();
+            nextPanda.setPrevAnimal(null);
+            orangutan = null;
+            return true;
+        }
+        return false;
     }
+    /**
+     * hitBy Panda
+     * <p>
+     *     This function overrides the base function and it is called when a chain
+     *     of Pandas is going through the exit.
+     * </p>
+     * @param  panda is the Thing which collided with this object.
+     * @return returns false, if Panda isn't in chain, and true if Panda succeeded to exit
+     */
     public boolean hitBy(Panda panda){
         if(!panda.isInChain()){
             return false;
@@ -19,6 +50,13 @@ public class Exit extends AThing {
         return true;
     }
 
+    /**
+     * nextTurn
+     * <p>
+     *     This function is called on every turn, if there is a panda to receive,
+     *     than it tries to move it through the exit.
+     * </p>
+     */
     public void nextTurn(){
         if(nextPanda != null){
             nextPanda.move(isOn);
