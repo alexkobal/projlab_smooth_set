0ae481609c5cc4f86058de9f11a1f9cde29f80cf
alexkobal <alexkobal90@gmail.com>
2019-04-23 22:26:35 +0200

    status

--- src/Program.java
+++ src/Program.java
@@ -36,7 +36,7 @@ public class Program {
 					try{
 						floor = Floor.deserialise(part[1]);
 						ctrl = new Controller(floor);
-						System.out.println(floor);
+						System.out.println(floor.status());
 					}catch (Exception e){
 						e.printStackTrace();
 					}
@@ -50,7 +50,7 @@ public class Program {
 						ctrl.animals.add(o);
 						ctrl.orangutans.add(o);
 						floor.getTile(Integer.parseInt(part[3])).setContains(o);
-						System.out.println(floor);
+						System.out.println(floor.status());
 					}
 					else if(part[1].compareTo("jpanda") == 0){
 						JumpingPanda jp = new JumpingPanda();
