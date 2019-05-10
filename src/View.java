f508a64ccdb8197879247fe32efb85af49756ea6
Peter Kecskes <kecskespeti@gmail.com>
2019-05-10 15:54:34 +0200

    Constructor and Deserialization methodes added

--- src/View.java
+++ src/View.java
@@ -1,10 +1,74 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class View
 {
-    Map<Tile, Node> nodes = new HashMap<>();
+    private Map<Tile, Node> nodes = new HashMap<>();
+    private Floor floor;
+    private int floorSize;
 
+
+    public View(String mapName, Floor _floor, int _floorSize)
+    {
+        floor = _floor;
+        floorSize = _floorSize;
+        Deserialize(mapName);
+    }
+
+    public void Deserialize(String mapName)
+    {
+        try
+        {
+            FileInputStream fis = new FileInputStream(mapName + ".txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            for(int i = 0; i <= floorSize; i++)
+            {
+                Tile key = floor.getTile(i);
+                String pos[] = br.readLine().split("\t");
+                Node n = new Node(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]));
+                nodes.put(key, n);
+            }
+
+            //Akkor, ha a floor esetleg hashmap lenne es kozvetlen indexelheto
+            /*
+            for(Map.Entry<Integer, Tile> entry : floor.tiles.entrySet())
+            {
+                Tile value = entry.getValue();
+                String pos[] = br.readLine().split("\t");
+                Node n = new Node(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]));
+                nodes.put(key, n);
+            }
+            */
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateDraw()
+    {
+        for(int i = 0; i <= floorSize; i++)
+        {
+            Tile key = floor.getTile(i);
+            //TODO
+        }
+
+        //Akkor, ha a floor esetleg hashmap lenne es kozvetlen indexelheto
+        /*
+        for(Map.Entry<Integer, Tile> entry : floor.tiles.entrySet())
+        {
+            Tile value = entry.getValue();
+            //TODO
+        }
+        */
+    }
 
     // állatok
     public void draw(Orangutan o)
