f6f917de9ae2eef52b8e359a8077ff1adc1ba271
Peter Kecskes <kecskespeti@gmail.com>
2019-05-10 15:53:47 +0200

    Constructor and getter Methodes added

--- src/Node.java
+++ src/Node.java
@@ -4,4 +4,19 @@ public class Node
     private int y;
 
     // etc.
+    public Node(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
 }
