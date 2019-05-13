21a8ef7ff260fec6b35a38d931e8a3b4f03f8d1a
alexkobal <alexkobal90@gmail.com>
2019-05-13 03:37:33 +0200

    Controller commented

--- src/Controller.java
+++ src/Controller.java
@@ -30,17 +30,41 @@ public class Controller {
 	 * </p>
 	 */
 	private ArrayList<Orangutan> orangutans;
-
+	/**
+	 * exit
+	 * <p>
+	 *     This flag tells if the game is over
+	 * </p>
+	 */
 	private boolean exit = false;
+	/**
+	 * Points
+	 * <p>
+	 *     Collected point counter
+	 * </p>
+	 */
 	private int Points = 0;
 
+	/**
+	 * Controller
+	 * <p>
+	 *     Default constructor. Initialises pandas, orangutans
+	 *     and sets instance
+	 * </p>
+	 */
 	private Controller(){
 		pandas = new ArrayList<>();
 		orangutans = new ArrayList<>();
 		instance = this;
 	}
 
-
+	/**
+	 * movePandaRandomly
+	 * <p>
+	 *     Moves one panda to random neighbor tile
+	 * </p>
+	 * @param panda panda to move
+	 */
 	public static void movePandaRandomly(Panda panda){
 		ArrayList<Tile> neighbors = panda.getIsOn().getNeighbors();
 		if(!neighbors.isEmpty()) {
@@ -49,6 +73,13 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * start
+	 * <p>
+	 *     Starts the game
+	 *     Runs tha game loop
+	 * </p>
+	 */
 	public void start(){
 		addAnimals(2,3);
 		while(!exit)
@@ -69,6 +100,12 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * entryNextTurn
+	 * <p>
+	 *     Does what should be done with entry in every turn
+	 * </p>
+	 */
 	public void entryNextTurn(){
 		if(Floor.getInstance().getEntry() != null)
 		{
@@ -86,6 +123,12 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * exitNextTurn
+	 * <p>
+	 *     Does what should be done with exit every turn
+	 * </p>
+	 */
 	public void exitNextTurn() {
 		if(Floor.getInstance().getExit() != null)
 		{
@@ -93,6 +136,12 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * moveOrangutans
+	 * <p>
+	 *     Iterates the orangutans and asks tha player to make the next move
+	 * </p>
+	 */
 	public void moveOrangutans(){
 		Object[] or = orangutans.toArray();
 		for(Object o : or){
@@ -104,12 +153,24 @@ public class Controller {
 		orangutans.removeIf(Orangutan::getIsDead);
 	}
 
+	/**
+	 * thingsNextTurn
+	 * <p>
+	 *     Iterates the notify-able things, and makes them notify
+	 * </p>
+	 */
 	public void thingsNextTurn(){
 		for(IPandaEffective ip : Floor.getInstance().getNotifiers()){
 			ip.effect();
 		}
 	}
 
+	/**
+	 * movePandas
+	 * <p>
+	 *     Moves all the pandas on the floor
+	 * </p>
+	 */
 	public void movePandas(){
 		Object[] panda = pandas.toArray();
 		for(Object p : panda){
@@ -118,20 +179,57 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * checkEnd
+	 * <p>
+	 *     Checks if the game is over
+	 * </p>
+	 * @return true if game over, false if still running
+	 */
 	public boolean checkEnd(){
 		if(pandas.isEmpty() || orangutans.isEmpty()) {return true;}
 		return false;
 	}
 
+	/**
+	 * removePanda
+	 * <p>
+	 *     Removes a panda from the game
+	 * </p>
+	 * @param p panda to remove
+	 */
 	public void removePanda(Panda p){
 		pandas.remove(p);
 	}
 
+	/**
+	 * removeOrangutan
+	 * <p>
+	 *     Removes an orangutan from the game
+	 * </p>
+	 * @param o orangutan to remove
+	 */
 	public void removeOrangutan(Orangutan o){
 		orangutans.remove(o);
 	}
 
+	/**
+	 * instance
+	 * <p>
+	 *     Singleton implementation
+	 *     Represents the one and only one instance of the Controller
+	 * </p>
+	 */
 	private static Controller instance = null;
+
+	/**
+	 * getInstance
+	 * <p>
+	 *     Creates a controller if not created
+	 *     Gets the controller if exists
+	 * </p>
+	 * @return controller instance
+	 */
 	public static Controller getInstance(){
 		if(instance != null){
 			return instance;
@@ -140,6 +238,14 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * placePandaRandomly
+	 * <p>
+	 *     Places a given panda on a random tile
+	 *     Needed while initialisation
+	 * </p>
+	 * @param panda panda to place
+	 */
 	private void placePandaRandomly(Panda panda){
 		int size = Floor.getInstance().getTiles().size();
 		int random = new Random().nextInt(size);
@@ -157,6 +263,14 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * addAnimals
+	 * <p>
+	 *     Adds animals to the game/floor
+	 * </p>
+	 * @param nOrangutans number of orangutans to add
+	 * @param nPandas number of pandas to add
+	 */
 	public void addAnimals(int nOrangutans, int nPandas){
 		for(int i = 0; i < nPandas; i++){
 			int random = new Random().nextInt(nPandas);
